d8ad71ec848eeb7fed472f309ff3d1581c8d8212
hpaziaud <107774544+hpaziaud@users.noreply.github.com>
2023-05-05 11:22:11 +0200

    Update ball.cpp

--- ball.cpp
+++ ball.cpp
@@ -1,57 +1,205 @@
-#include "ball.h"
-#include <QPainter>
-#include <QTimer>
+#include "Ball.h"
+#include "Player.h"
 
-Ball::Ball(QWidget *parent) : QWidget(parent)
+bool Ball::isRandInitialized = false;
+
+
+double Ball::randomizeAngle()
 {
-    x = 0;
-    y = 0;
-    dx = 1;
-    dy = 1;
+	if (!isRandInitialized) {
+		srand(time(NULL));
+		isRandInitialized = true;
+	}
 
-    QTimer *timer = new QTimer(this);
-    connect(timer, SIGNAL(timeout()), this, SLOT(move()));
-    timer->start(10);
+
+	return (((double)rand() / (double)RAND_MAX) * 60.0) - 30.0;
 }
 
-void Ball::paintEvent(QPaintEvent *event)
+void Ball::setAngle(double angle)
 {
-    QPainter painter(this);
-
-    painter.setRenderHint(QPainter::Antialiasing, true);
-    painter.setBrush(Qt::red);
-    painter.setPen(Qt::black);
-    painter.drawEllipse(x, y, 20, 20);
+	direction.x = sin(angle);
+	direction.y = -cos(angle);
 }
 
-void Ball::move()
+Ball::Ball(int x, int y, int radius, float speed)
 {
-    x += dx;
-    y += dy;
+	position.x = x;
+	position.y = y;
+	oldPosition = position;
+	shape.setFillColor(sf::Color(sf::Color::White));
+	this->radius = radius;
+	shape.setRadius(radius);
+	this->speed = speed;
 
-    if (x < 0)
-    {
-        x = 0;
-        dx = -dx;
-    }
+	setAngle(randomizeAngle());
 
-    if (x > width() - 20)
-    {
-        x = width() - 20;
-        dx = -dx;
-    }
+}
 
-    if (y < 0)
-    {
-        y = 0;
-        dy = -dy;
-    }
+Ball::~Ball()
+{
+}
 
-    if (y > height() - 20)
-    {
-        y = height() - 20;
-        dy = -dy;
-    }
+void Ball::move(float ellapsedTime)
+{
+	position += ellapsedTime * speed * direction;
+}
 
-    update();
+void Ball::draw(sf::RenderWindow& window)
+{
+	shape.setPosition(position);
+	window.draw(shape);
+}
+
+float Ball::getSpeed()
+{
+	return speed;
+}
+
+void Ball::setSpeed(float newSpeed)
+{
+	speed = newSpeed;
+}
+
+void Ball::setPosition(sf::Vector2f newPosition)
+{
+	position = newPosition;
+	oldPosition = position;
+}
+
+sf::Vector2f Ball::getPosition()
+{
+	return position;
+}
+
+void Ball::setDirection(sf::Vector2f newDirection)
+{
+	direction = newDirection;
+}
+
+void Ball::update(float deltaTime, Player& player, sf::RenderWindow& window)
+{
+	position += direction * speed * deltaTime;
+	shape.setPosition(position);
+
+	manageCollisionWith(player, window);
+
+	// Si la balle reste collée au joueur, réinitialise sa position
+	if (collisionCount > 5) {
+		position.y = player.getPosition().y - 2 * radius;
+		collisionCount = 0;
+	}
+}
+
+void Ball::manageCollisionWith(Player& player, sf::RenderWindow& window)
+{
+	float MAX_ANGLE = 45;
+	const int MARGE = 2;
+
+	// Vérifie si la balle touche le joueur
+	if (position.y + 2 * radius >= player.getPosition().y &&
+		position.y + 2 * radius <= player.getPosition().y + player.getSize().y &&
+		position.x + 2 * radius >= player.getPosition().x &&
+		position.x <= player.getPosition().x + player.getSize().x)
+	{
+		// Calcule la position relative de la balle par rapport au joueur
+		double relativeIntersectX = position.x + radius - player.getPosition().x - player.getSize().x / 2.0;
+
+		// Normalise la position relative de la balle
+		double normalizedRelativeIntersectionX = relativeIntersectX / (player.getSize().x / 2.0);
+
+		// Calcule l'angle de rebond de la balle
+		double angle = normalizedRelativeIntersectionX * MAX_ANGLE;
+
+		// Change la direction de la balle en fonction de l'angle de rebond
+		setAngle(angle);
+
+		// Déplace la balle en dehors du joueur
+		position.y = player.getPosition().y - 2 * radius - 0.1f;
+		direction.y = -std::abs(direction.y);
+	}
+
+	// Vérifie si la balle touche le bord supérieur de l'écran
+	if (position.y <= 0)
+	{
+		// Inverse la direction sur l'axe y :
+		direction.y = std::abs(direction.y);
+	}
+
+	// Vérifie si la balle touche le bord inférieur de l'écran
+	if (position.y >= (window.getSize().y) - 2 * radius)
+	{
+		// Inverse la direction sur l'axe y :
+		direction.y = -std::abs(direction.y);
+	}
+
+	// Vérifie si la balle touche le bord gauche de l'écran
+	if (position.x <= 0)
+	{
+		// Inverse la direction sur l'axe x :
+		direction.x = std::abs(direction.x);
+	}
+
+	// Vérifie si la balle touche le bord droit de l'écran
+	if (position.x >= (window.getSize().x) - 2 * radius)
+	{
+		// Inverse la direction sur l'axe x :
+		direction.x = -std::abs(direction.x);
+	}
+}
+
+void Ball::manageCollisionWithBrick(Brick* brick)
+{
+	sf::FloatRect ballBounds = shape.getGlobalBounds();
+	sf::FloatRect brickBounds = brick->getShape().getGlobalBounds();
+
+	// Vérifie si la balle touche la brique
+	if (ballBounds.intersects(brickBounds))
+	{
+		// Calcule la position relative de la balle par rapport à la brique
+		double intersectX = position.x + radius - brick->getPosition().x;
+		double intersectY = position.y + radius - brick->getPosition().y;
+		double deltaX = brick->getSize().x / 2.0 - std::abs(intersectX);
+		double deltaY = brick->getSize().y / 2.0 - std::abs(intersectY);
+
+		// Inverse la direction de la balle en fonction de l'axe de la collision
+		if (deltaX > deltaY)
+		{
+			// Collision horizontale
+			direction.x *= -1;
+			position.x += (intersectX > 0 ? deltaX : -deltaX);
+		}
+		else
+		{
+			// Collision verticale
+			direction.y *= -1;
+			position.y += (intersectY > 0 ? deltaY : -deltaY);
+		}
+
+		// Réduit la santé de la brique et change sa couleur
+		brick->hit();
+
+		// Changer la direction de la balle immédiatement après la collision
+		// en fonction de la position de la brique relative à la balle
+		if (intersectX > 0)
+		{
+			// La balle touche la brique sur le côté droit
+			direction.x = std::abs(direction.x);
+		}
+		else
+		{
+			// La balle touche la brique sur le côté gauche
+			direction.x = -std::abs(direction.x);
+		}
+
+		if (intersectY > 0)
+		{
+			// La balle touche la brique en bas
+			direction.y = std::abs(direction.y);
+		}
+		else
+		{
+			// La balle touche la brique en haut
+			direction.y = -std::abs(direction.y);
+		}
+	}
 }
